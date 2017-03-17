10751cb1fecf103fd1f767ed55f8aafb1831fd3b
Adrian Daras <getpingui@gmail.com>
2017-03-17 07:33:48 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+  attr_reader :booty, :name, :type
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
+
+  def initialize(params)
+    @booty = params[:booty]
+    @name = params[:name]
+    @type = params[:type]
+
+    @@all << self
+  end
 end
