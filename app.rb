694c1d832d4c69eff788f59d9aef1cb1647ab8ef
robertjosephwayne <robertjwayne@gmail.com>
2017-04-28 12:13:23 -0400

    Add post '/pirates' route

--- app.rb
+++ app.rb
@@ -3,8 +3,16 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
+    get '/' do
+      erb :root
+    end
+
     get '/new' do
       erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      erb :'pirates/show'
     end
 
   end
