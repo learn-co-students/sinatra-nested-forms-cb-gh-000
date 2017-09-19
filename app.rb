6d48fdd3ae0ea12ab9a9644010e6560a783d6a69
Hayden Betts <haydenbetts@gmail.com>
2017-09-19 02:20:17 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,30 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+
+      # syntax for erb into a new path
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+
+    # intialize using mass assignment from a pirate hash
+    @pirate = Pirate.new(params[:pirate])
+
+      # initialize using mass assignment from a ship hash
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+
+      @ships = Ship.all
+
+      erb :'pirates/show'
+    end
 
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  def initialize(args)
+    @name = args[:name]
+    @weight = args[:weight]
+    @height = args[:height]
+  end
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
 end
