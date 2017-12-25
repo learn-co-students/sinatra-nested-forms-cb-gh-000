863df5cdc2e8b726fd1819133fb183d13efa840a
Kid Coder <toddacodda@gmail.com>
2017-12-25 12:56:01 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
 class Pirate
+  @@all = []
+  attr_accessor :name, :weight, :height, :ships
+  def initialize(name, weight, height, ships = [])
+    @name   = name
+    @weight = weight
+    @height = height
+    @ships = ships.map { |e| Ship.new(e) }
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
