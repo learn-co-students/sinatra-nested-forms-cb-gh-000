5a2d58e0072289c30c2734538860cf2e69b7c4b1
Shlok Joshi <shlokjoshi@gmail.com>
2018-04-08 07:19:58 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,17 @@
-class Ship
+class Pirate
+	attr_accessor :name, :weight, :height
+	SHIPS = []
+	def initialize(name, type, booty)
+		@name, @type, @booty = name, type, booty
+		SHIPS << self
+	end
+
+	def self.all
+		SHIPS
+	end
+
+	def self.clear
+		SHIPS.destroy
+	end
+
 end
