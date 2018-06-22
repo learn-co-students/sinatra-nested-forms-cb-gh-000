08bc2176a219ccf7a2f45edcc969f35e9270efb0
Berry  Semexan <bsemexan@gmail.com>
2018-06-22 01:27:29 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+  attr_reader :name, :weight, :height
+
+  PIRATES = []
+
+  def initialize(arg)
+    @name = arg[:name]
+    @weight = arg[:weight]
+    @height = arg[:height]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
 end
